17a81d80c236efc8864405dbf1796d9949f28abe
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-19 09:26:19 -0300

    feat: Entity Pagamento, Status, application-test, update application.properties. Repository: PagamentoRepository

---
+++ ms-pagamento/src/main/java/com/github/kinderenge/ms_pagamento/entity/Pagamento.java
@@ -0,0 +1,34 @@
+package com.github.kinderenge.ms_pagamento.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+
+@Entity
+@Table(name = "tb_pagaemnto")
+public class Pagamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private BigDecimal valor;
+    private String nome;                //nome no cartão
+    private String numeroDoCartao;      //xxxx xxxx xxxx xxxx
+    private String validade;            //MM/AA
+    private String codigoDeSeguranca;   // xxx
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private Status status;
+    @Column(nullable = false)
+    private Long pedidoId;
+    @Column(nullable = false)
+    private long formaDePagamentoId; //1- dinheiro | 2 - cartão | 3 - pix
+}
---
+++ ms-pagamento/src/main/java/com/github/kinderenge/ms_pagamento/entity/Status.java
@@ -0,0 +1,8 @@
+package com.github.kinderenge.ms_pagamento.entity;
+
+public enum Status {
+
+    CRIADO,
+    CONFIRMADO,
+    CANCELADO
+}
