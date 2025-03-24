a529e4fb33f0de9887f44ddeb4ecf9662ae6ca46
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-24 11:17:35 -0300

    feat: Add ResourceNotFoundException, PagamentoService getById

---
+++ ms-pagamento/src/main/java/com/github/kinderenge/ms_pagamento/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.github.kinderenge.ms_pagamento.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+    public ResourceNotFoundException(String message){
+        super(message);
+    }
+}
--- ms-pagamento/src/main/java/com/github/kinderenge/ms_pagamento/service/PagamentoService.java
+++ ms-pagamento/src/main/java/com/github/kinderenge/ms_pagamento/service/PagamentoService.java
@@ -2,6 +2,7 @@ package com.github.kinderenge.ms_pagamento.service;
 
 import com.github.kinderenge.ms_pagamento.dto.PagamentoDTO;
 import com.github.kinderenge.ms_pagamento.entity.Pagamento;
+import com.github.kinderenge.ms_pagamento.exceptions.ResourceNotFoundException;
 import com.github.kinderenge.ms_pagamento.repository.PagamentoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,5 +19,11 @@ public class PagamentoService {
     public List<PagamentoDTO> getAll(){
         List<Pagamento>pagamentos = repository.findAll();
         return pagamentos.stream().map(PagamentoDTO::new).collect(Collectors.toList());
+    }
+
+    @Transactional(readOnly=true)
+    public PagamentoDTO getById(Long id){
+        Pagamento entity = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado. ID: "+ id));
+        return new PagamentoDTO(entity);
     }
 }
