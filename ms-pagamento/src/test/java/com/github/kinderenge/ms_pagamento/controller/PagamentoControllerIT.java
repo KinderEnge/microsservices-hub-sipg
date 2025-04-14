e6a01444a401c2e648645bbe83923e00b08f64f6
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-14 11:38:12 -0300

    integration test: createPagamento - Controller

--- ms-pagamento/src/test/java/com/github/kinderenge/ms_pagamento/controller/PagamentoControllerIT.java
+++ ms-pagamento/src/test/java/com/github/kinderenge/ms_pagamento/controller/PagamentoControllerIT.java
@@ -13,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -52,6 +53,15 @@ public class PagamentoControllerIT {
     @Test
     public void getByIdShouldThrowResourceNotFoundExceptionWhenIdDoesNotExist() throws Exception{
         mockMvc.perform(get("/pagamentos/{id}", nonExistingId).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isNotFound()).andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void createShouldReturnPagamentoDTO() throws Exception{
+        pagamentoDTO = Factory.createNewPagamentoDTO();
+
+        String jsonRequestBody = objectMapper.writeValueAsString(pagamentoDTO);
+
+        mockMvc.perform(post("/pagamentos").content(jsonRequestBody).contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON)).andDo(print()).andExpect(status().isCreated()).andExpect(header().exists("Location")).andExpect(jsonPath("$.id").exists()).andExpect(jsonPath("$.valor").exists()).andExpect(jsonPath("$.nome").value(pagamentoDTO.getNome())).andExpect(jsonPath("$.status").value("CRIADO"));
     }
 
 }
