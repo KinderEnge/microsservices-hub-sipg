36abd2bb84f49477c1751b3dd5a452157ce69b04
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-28 11:42:03 -0300

    feat: PedidoRepository - Repository

---
+++ ms-pedido/src/main/java/com/github/kinderenge/ms_pedido/repositories/PedidoRepository.java
@@ -0,0 +1,8 @@
+package com.github.kinderenge.ms_pedido.repositories;
+
+import com.github.kinderenge.ms_pedido.entities.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
+    
+}
