ef9f4e7cfd4907583f40edf0192a3ba063d09ffc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-04-30 08:42:07 -0300

    feat: ItemDoPedidoRepository - Repositories

---
+++ ms-pedido/src/main/java/com/github/kinderenge/ms_pedido/repositories/ItemDoPedidoRepository.java
@@ -0,0 +1,7 @@
+package com.github.kinderenge.ms_pedido.repositories;
+
+import com.github.kinderenge.ms_pedido.entities.ItemDoPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemDoPedidoRepository extends JpaRepository<ItemDoPedido, Long> {
+}
